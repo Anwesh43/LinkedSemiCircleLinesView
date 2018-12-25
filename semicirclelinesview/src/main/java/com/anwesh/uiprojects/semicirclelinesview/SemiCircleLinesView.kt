f7d6c3facc3f4a77879c3d212b526e4cdce34c04
Anwesh43 <anweshthecool0@gmail.com>
2018-12-25 11:58:09 +0530

    adding SemiCircleLines class

--- semicirclelinesview/src/main/java/com/anwesh/uiprojects/semicirclelinesview/SemiCircleLinesView.kt
+++ semicirclelinesview/src/main/java/com/anwesh/uiprojects/semicirclelinesview/SemiCircleLinesView.kt
@@ -164,7 +164,31 @@ class SemiCircleLinesView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class SemiCircleLines(var i : Int) {
+
+        private val root : SCLNode = SCLNode(0)
+        private var curr : SCLNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
